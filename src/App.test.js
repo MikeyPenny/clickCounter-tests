c1f4b5a9f361b96a86b8b85d9a919e7c9b0bbf09
Miguel Sandoval <miguelship3@gmail.com>
2021-04-07 18:17:24 +0200

    Add before eject

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import Enzyme, { configure, shallow } from "enzyme";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+
+configure({ adapter: new EnzymeAdapter() });
+
+
+test("renders without errors", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
+
+test("renders increment button", () => {
+
+});
+
+test("shows counter display", () => {
+
+});
+
+test("counter display starts at 0", () => {
+
+});
+
+test("button click increments counter display", () => {
+
 });
